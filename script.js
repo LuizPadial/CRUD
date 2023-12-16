a6a3c354ead991e88f54b1dc57a1b661804bc8d5
Luiz Padial <134821865+LuizPadial@users.noreply.github.com>
2023-12-16 14:38:42 -0300

    Update script.js
    Adicionado dois tratamentos para cada status<br>
    alterado nome das variáveis para o código ficar mais legível.

--- script.js
+++ script.js
@@ -1,41 +1,52 @@
-//acessar o formulario e guardar os dados dentro de uma variavel
+// Acessar o formulário e guardar os dados dentro de uma variável
 const formulario = document.querySelector("form");
-const Inome = document.querySelector(".nome");
-const Iemail = document.querySelector(".email");
-const Isenha = document.querySelector(".senha");
-const Itel = document.querySelector(".tel");
+const inome = document.querySelector(".nome");
+const iemail = document.querySelector(".email");
+const isenha = document.querySelector(".senha");
+const itelefone = document.querySelector(".telefone");
 
-//método post
-function cadastrar (){
-    fetch("http://localhost:8080/cadastrar",
-    {
+// Método post
+function cadastrar() {
+    fetch("http://localhost:8080/usuarios", {
         headers: {
-            'Accept':'application/json',
-            'Content-Type': 'application/json'
+            "Accept": "application/json",
+            "Content-Type": "application/json"
         },
-        method:"POST",
-        body: JSON.stringify({       
-            nome: Inome.value,
-            email: Iemail.value,
-            senha: Isenha.value,
-            telefone: Itel.value
+        method: "POST",
+        body: JSON.stringify({
+            nome: inome.value,
+            email: iemail.value,
+            senha: isenha.value,
+            telefone: itelefone.value
         })
     })
-    .then(function (res) {console.log(res) })
-    .catch(function (res) {console.log(res) })  
+    .then(function (res) {
+        if (!res.ok) {
+            throw new Error(`Erro na requisição: ${res.status} - ${res.statusText}`);
+        }
+        return res.json();
+    })
+    .then(function (data) {
+        console.log("Cadastro realizado com sucesso:", data);
+        // Adicione aqui o código para fornecer feedback visual ao usuário (ex: exibir uma mensagem)
+        limpar(); // Limpar o formulário após o sucesso
+    })
+    .catch(function (error) {
+        console.error("Erro ao cadastrar:", error);
+        // Adicione aqui o código para fornecer feedback visual ao usuário em caso de erro
+    });
 }
 
-//função de limpar o formulário
-function limpar () {
-    Inome.value = ""
-    Iemail.value =""
-    Isenha.value = ""
-    Itel.value = ""
+// Função de limpar o formulário
+function limpar() {
+    inome.value = "";
+    iemail.value = "";
+    isenha.value = "";
+    itelefone.value = "";
 }
 
-//evento do botão cadastrar e transformar os dados em um objeto do tipo JSON
-formulario.addEventListener('submit', function(event){
+// Evento do botão cadastrar e transformar os dados em um objeto do tipo JSON
+formulario.addEventListener("submit", function(event) {
     event.preventDefault();
-    limpar();
     cadastrar();
 });
